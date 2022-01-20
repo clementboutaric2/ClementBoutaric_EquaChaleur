6b1ddd946b855f602021d145ca9ea91ea781d1d2
clementboutaric2 <93884878+clementboutaric2@users.noreply.github.com>
2022-01-20 21:19:10 +0100

    c'est le main

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include "matrice.hpp"
+#include "corps_test.hpp"
+#include "resolution.hpp"
+#include "Euler_explicite.hpp"
+#include "graphisme.hpp"
+
+
+
+int main()
+{
+    const double duree = 1; //s
+    const double delta_temp = 0.0001; //s
+    const double longueur = 1; //m
+    const double delta_spat = 0.01;
+    const std::vector<double> D (static_cast<int>(longueur/delta_spat),1);
+
+    matrice<double> CI (static_cast<int>(longueur/delta_spat), 1);
+
+    for (int i = 1; i <= CI.Size()[0]; i++)
+    {
+        CI.setValue(i,1, 0.5 + std::sin(2*3.14*(i-1)*delta_spat) - 0.5*std::cos(2*3.14*(i-1)*delta_spat));
+    }
+
+    matrice<double> K  = MatriceConductivite(duree, delta_temp, longueur, delta_spat, D);
+
+    std::vector<matrice<double>> VT = VecteurTemporel(duree, delta_temp, CI, longueur, delta_spat);
+
+    Euler_Explicite<double>(&VT, K, delta_temp);
+    
+    InscriptionDansFichier(&VT);
+}
