6a0e4f598b58ca8cd9e0a95c321a3b8a8d7de1f9
clementboutaric2 <93884878+clementboutaric2@users.noreply.github.com>
2022-01-20 21:08:14 +0100

    j'avais oublié de mettre le code

--- Euler_Explicit.hpp
+++ Euler_Explicit.hpp
@@ -1,1 +1,16 @@
+#pragma once
 
+#include "matrice.hpp"
+#include <vector>
+
+template<typename T>
+void Euler_Explicite (std::vector<matrice<T>>* VectT, const matrice<T>& K, const T& deltaTemp)
+{
+
+    matrice<T> dtK (deltaTemp*K);
+
+    for (auto it = std::begin(*VectT)+1; it != std::end(*VectT); it++)
+    {
+        *it = *(it-1) + dtK*(*(it-1));
+    }
+}
