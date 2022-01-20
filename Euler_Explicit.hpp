36e79e086ae1a5128e07d2ba40b16da22bbc8d2d
clementboutaric2 <93884878+clementboutaric2@users.noreply.github.com>
2022-01-20 23:01:56 +0100

    passage par référence plutôt que par pointeur.

--- Euler_Explicit.hpp
+++ Euler_Explicit.hpp
@@ -4,12 +4,12 @@
 #include <vector>
 
 template<typename T>
-void Euler_Explicite (std::vector<matrice<T>>* VectT, const matrice<T>& K, const T& deltaTemp)
+void Euler_Explicite (std::vector<matrice<T>>& VectT, const matrice<T>& K, const T& deltaTemp)
 {
 
     matrice<T> dtK (deltaTemp*K);
 
-    for (auto it = std::begin(*VectT)+1; it != std::end(*VectT); it++)
+    for (auto it = std::begin(VectT)+1; it != std::end(VectT); it++)
     {
         *it = *(it-1) + dtK*(*(it-1));
     }
